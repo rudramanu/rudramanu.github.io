da62a0b825e2084fa36928c7cc8fda0f78254f1f
Rudra <merudramanu@gmail.com>
2023-02-13 21:30:48 +0530

    added final

---
+++ index.js
@@ -0,0 +1,6 @@
+const navResume = document.getElementById("resume-button-1");
+
+navResume.addEventListener("click", () => {
+  window.location =
+    "https://drive.google.com/uc?export=download&id=1-oj-xMFTdGJLW3igB5dMVMm9BwlU_XM8";
+});
