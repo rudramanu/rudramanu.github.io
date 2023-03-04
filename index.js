19dbf93063a8c753d65becdaf3856c37d25e9087
Rudra <merudramanu@gmail.com>
2023-03-04 13:24:37 +0530

    added updated portfolio

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
-const navResume = document.getElementById("resume-button-1");
+let sidemenu = document.getElementById("sidemenu");
 
-navResume.addEventListener("click", () => {
-  window.location =
-    "https://drive.google.com/uc?export=download&id=1-oj-xMFTdGJLW3igB5dMVMm9BwlU_XM8";
+function openmenu() {
+  sidemenu.style.right = "0";
+}
+function closemenu() {
+  sidemenu.style.right = "-200px";
+}
+
+document.getElementById("resume-link-1").addEventListener("click", () => {
+  // console.log("OPENinig.....")
+  window.location.assign(
+    "https://drive.google.com/file/d/1-oj-xMFTdGJLW3igB5dMVMm9BwlU_XM8/view?usp=share_link",
+    "_blank"
+  );
+});
+
+document.getElementById("resume-link-2").addEventListener("click", () => {
+  // console.log("OPENinig.....")
+  window.location.assign(
+    "https://drive.google.com/file/d/1-oj-xMFTdGJLW3igB5dMVMm9BwlU_XM8/view?usp=share_link",
+    "_blank"
+  );
 });
